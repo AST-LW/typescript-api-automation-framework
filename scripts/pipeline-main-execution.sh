eb1810cb96fb03f3f92afe0e09dbbee72a8c5dbc
AST-LW <walkthroughfrom2020@gmail.com>
2024-04-09 20:43:42 +0530

    minor fix

--- scripts/pipeline-main-execution.sh
+++ scripts/pipeline-main-execution.sh
@@ -5,11 +5,11 @@ SUITE=$SUITE ENV=$ENV INSTANCES=$INSTANCES npx jest
 
 # Execute generate-allure-report.sh script
 chmod +x scripts/util-scripts/generate-allure-report.sh
-scripts/generate-allure-report.sh
+scripts/util-scripts/generate-allure-report.sh
 
 # Execute extraction of test summary results
 npx ts-node src/utils/test-summary-extractor.ts
 
 # Execute slack-notification.sh script
 chmod +x scripts/util-scripts/slack-notification.sh
-scripts/slack-notification.sh
+scripts/util-scripts/slack-notification.sh
