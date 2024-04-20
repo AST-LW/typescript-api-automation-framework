e9c9a9bd8b3fa8e909f22e55de38ac22982e2500
AST-LW <walkthroughfrom2020@gmail.com>
2024-04-20 23:51:35 +0530

    added more files

---
+++ tests/regression/users/create-user_7.spec.ts
@@ -0,0 +1,51 @@
+import Container from "typedi";
+
+import { UserClient } from "../../../src/clients/user.client";
+import { addLogsToAllure, loggerInitializationHook } from "../../../setup/within-hooks";
+import { Actions } from "../../../src/actions/action-lookup";
+import { RequestDataGenerator } from "../../../src/utils/request-data-generator";
+import { SuccessfulUserCreationRequestModel, WithoutEmailRequestModel } from "../../../src/models/request/user";
+
+let userClient: UserClient;
+
+beforeEach(() => {
+    // Logger initialization
+    loggerInitializationHook(expect.getState().currentTestName as string);
+
+    //  Fetching the client objects
+    userClient = Container.get(UserClient);
+});
+
+afterEach(() => {
+    addLogsToAllure(expect.getState().currentTestName as string);
+});
+
+describe("User Suite", () => {
+    it("@TEST_ID-3022 - Create a new user successfully", async () => {
+        const data = RequestDataGenerator.createUserPayload() as SuccessfulUserCreationRequestModel;
+
+        const response = await Actions.user.successfulUserCreation(data);
+
+        expect(response.statusCode).toBe(201);
+        expect(response.data?.user_id).toBeTruthy();
+        expect(response.data?.access_token).toBeTruthy();
+    });
+
+    it("@TEST_ID-3023 - Unsuccessful creation of user, due to missing email field in the payload", async () => {
+        const data = RequestDataGenerator.createUserPayload(["email"]) as WithoutEmailRequestModel; // Exclude email from the payload
+
+        const response = await Actions.user.createUserWithoutEmail(data);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.data?.error).toBe("Username, password, and email are required.");
+    });
+
+    it("@TEST_ID-3024 - Unsuccessful creation of user, with no payload", async () => {
+        const data: any = {};
+
+        const response = await Actions.user.createUserWithoutEmail(data);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.data?.error).toBe("Username, password, and email are required.");
+    });
+});
---
+++ tests/regression/users/delete-user_11.spec.ts
@@ -0,0 +1,28 @@
+import Container from "typedi";
+
+import { UserClient } from "../../../src/clients/user.client";
+import { addLogsToAllure, loggerInitializationHook } from "../../../setup/within-hooks";
+import { Actions } from "../../../src/actions/action-lookup";
+
+let userClient: UserClient;
+
+beforeEach(() => {
+    // Logger initialization
+    loggerInitializationHook(expect.getState().currentTestName as string);
+
+    //  Fetching the client objects
+    userClient = Container.get(UserClient);
+});
+
+afterEach(() => {
+    addLogsToAllure(expect.getState().currentTestName as string);
+});
+
+describe("User Suite", () => {
+    it("@TEST_ID-1214 - Successful user deletion", async () => {
+        const response = await Actions.user.successfulUserDeletion();
+
+        expect(response.statusCode).toBe(200);
+        expect(response.data?.message).toBe("User deleted successfully");
+    });
+});
